eafbc33dd2f4fe9f6a00278893e9d785d60aad33
sbagaitkar <sayalipeshave@gmail.com>
2024-12-22 11:29:06 -0500

    New Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState} from 'react'
+import "./App.css";
+//code for counter functionality, counter will increase by 1 when user hits '+' button. Counter will decrease by 1 when user hits '-' button.
+export default function App() {
+ //const [upCount, increaseCounter] = useState();
+ //const [downCount, decreaseCounter] = useState();
+ const [num,setNum] = useState(5);
+ 
+ const increaseCounter = () => {
+    setNum(num+1);
+ }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const decreaseCounter = () => {
+    setNum(num-1);
+ }
+ 
+ return (
+    <div>
+    <div className="row" >
+        <h1> Counter </h1>
+     </div>   
+
+        <div className = "row1">
+        <p><button onClick={increaseCounter}> + </button></p>
+        <p className="number"><b>{ num }</b></p>
+        <p><button onClick={decreaseCounter}> - </button></p>
+        </div>
+
     </div>
-  );
+  )
 }
-
-export default App;
